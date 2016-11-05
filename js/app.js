3a090a7675015922147c3e8defdad0c1b4fcb9e4
aramdya23 <aramvardanyans@gmail.com>
2016-11-05 14:57:57 +0400

    many changes

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by aram on 11/5/16.
+ */
+
+var App ={
+    Models : {},
+    Views : {},
+    Collections : {}
+
+
+};
+
+App.Models.bookModel = Backbone.Model.extend({
+
+    title : "srst"
+
+});
